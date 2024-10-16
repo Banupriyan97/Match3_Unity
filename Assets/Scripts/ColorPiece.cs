bac8f7495a0628b17044d9d4db41be907b564633
Banupriyan Raveendran <banupriyan@4axissolutions.com>
2024-10-16 22:10:27 +0530

    Load addressable sprites by address

--- Assets/Scripts/ColorPiece.cs
+++ Assets/Scripts/ColorPiece.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Match3
 {
@@ -27,19 +29,48 @@ namespace Match3
         private SpriteRenderer _sprite;
         private Dictionary<ColorType, Sprite> _colorSpriteDict;
 
-        private void Awake ()
+        [SerializeField]
+        string m_yellowFishSpriteAddress;
+        private AsyncOperationHandle<Sprite> m_FishLoadOpHandle;
+
+        private void OnEnable()
+        {
+            m_FishLoadOpHandle = Addressables.LoadAssetAsync<Sprite>(m_yellowFishSpriteAddress);
+            m_FishLoadOpHandle.Completed += OnFishSpriteLoadComplete;
+        }
+
+        private void OnDisable()
+        {
+            m_FishLoadOpHandle.Completed -= OnFishSpriteLoadComplete;
+        }
+
+        private void Awake()
         {
             _sprite = transform.Find("piece").GetComponent<SpriteRenderer>();
 
+            UpdateColorSpriteDict();
+        }
+
+        void UpdateColorSpriteDict()
+        {
             // instantiating and populating a Dictionary of all Color Types / Sprites (for fast lookup)
             _colorSpriteDict = new Dictionary<ColorType, Sprite>();
 
             for (int i = 0; i < colorSprites.Length; i++)
             {
-                if (!_colorSpriteDict.ContainsKey (colorSprites[i].color))
+                if (!_colorSpriteDict.ContainsKey(colorSprites[i].color))
                 {
                     _colorSpriteDict.Add(colorSprites[i].color, colorSprites[i].sprite);
                 }
+            }
+            UpdateSpritewrtColor();
+        }
+
+        void UpdateSpritewrtColor()
+        {
+            if (_colorSpriteDict.ContainsKey(_color))
+            {
+                _sprite.sprite = _colorSpriteDict[_color];
             }
         }
 
@@ -52,6 +83,16 @@ namespace Match3
                 _sprite.sprite = _colorSpriteDict[newColor];
             }
         }
-	
+
+        void OnFishSpriteLoadComplete(AsyncOperationHandle<Sprite> asyncOperationHandle)
+        {
+            for(int i = 0; i < NumColors; i++)
+            {
+                if(colorSprites[i].color == ColorType.Yellow) {
+                    colorSprites[i].sprite = asyncOperationHandle.Result;
+                }
+            }
+            UpdateColorSpriteDict();
+        }
     }
 }
